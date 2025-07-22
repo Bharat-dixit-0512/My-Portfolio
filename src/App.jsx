31beb2dfdf120282c5d39d850c91c1836c6b1de3
Bharat-dixit-0512 <dixitbt251@gmail.com>
2025-07-23 02:13:08 +0530

    my-portfolio

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Projects from "./components/Projects"
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<><Navbar/><Home/></>
+    },
+    {
+      path:'/About',
+      element:<><Navbar/><About/></>
+    },
+    {
+      path:'/Contact',
+      element:<><Navbar/><Contact/></>
+    },
+    {
+      path:'/Projects',
+      element:<><Navbar/><Projects/></>
+    },
+    {
+      path:'/Certificate',
+      element:<><Navbar/><Projects/></>
+    },
+   
+  ])
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+  
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-white'>
+      I am You
+    </div>
+  )
+}
+
+export default About
